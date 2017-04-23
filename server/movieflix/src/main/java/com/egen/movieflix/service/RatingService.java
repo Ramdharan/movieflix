97ff339c596c35132ff30856b11227585a6dff6b
Ramdharan <rdharan09@gmail.com>
2017-04-23 13:04:48 -0700

    feat(module-api): add new service methods

--- server/movieflix/src/main/java/com/egen/movieflix/service/RatingService.java
+++ server/movieflix/src/main/java/com/egen/movieflix/service/RatingService.java
@@ -5,6 +5,11 @@ import java.util.Map;
 public interface RatingService {
 	public Map<String, Object> insertRating(String catalogid, Double rating);
 
-	public Double getAverageRating(String id);
+	public Map<String,Double> getAverageRating(String id);
+	
+	public Map<String,Object> getUserRating(String catalogid);
+	
+
+	public Map<String,Double> updateUserRatings(Double user_rating, String ratingid);
 
 }
