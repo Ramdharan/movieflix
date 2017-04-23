91acdea1b337edbd5e38616025ea51d67c5d6159
Ramdharan <rdharan09@gmail.com>
2017-04-23 12:43:14 -0700

    feat(module-api): add new end points

--- server/movieflix/src/main/java/com/egen/movieflix/controller/RatingController.java
+++ server/movieflix/src/main/java/com/egen/movieflix/controller/RatingController.java
@@ -16,7 +16,7 @@ import com.egen.movieflix.service.RatingService;
 /**
  * Handles mapping request related to Ratings
  * 
- * @author ramdharandonda
+ *
  *
  */
 @RestController
@@ -26,18 +26,57 @@ public class RatingController {
 	@Autowired
 	public RatingService ratingService;
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/insert", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Map<String, Object> insertNewRating(@RequestBody Object data) {
 		// Spring will automatically convert JSON String to LinkedHashMap.
 		// We can also create a POJO with all the fields in JSON.
+
+		System.out.println("Insert new rating....");
 		LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) data;
 		String catalogid = (String) map.get("catalogid");
-		Double rating = (Double) map.get("rating");
+		double rating = getRating(map.get("rating"));
+
+		// double rating = (double) map.get("rating");
 		return ratingService.insertRating(catalogid, rating);
 	}
 
 	@RequestMapping(value = "/getavg", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Double getAverageRatings(@RequestParam("catalogid") String id) {
+	public Map<String, Double> getAverageRatings(@RequestParam("catalogid") String id) {
+
 		return ratingService.getAverageRating(id);
+	}
+
+	@RequestMapping(value = "/getuserrating", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Map<String, Object> getUserRating(@RequestParam("catalogid") String catalogid) {
+		return ratingService.getUserRating(catalogid);
+	}
+
+	@RequestMapping(value = "/updaterating", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Map<String, Double> updateUserRating(@RequestBody Object data) {
+		LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) data;
+		String ratingid = (String) map.get("ratingid");
+		double rating = getRating(map.get("rating"));
+
+		return ratingService.updateUserRatings(rating, ratingid);
+	}
+
+	/**
+	 * Converts user rating to double
+	 * 
+	 * @param ratingObj
+	 * @return
+	 */
+
+	public double getRating(Object ratingObj) {
+		double rating;
+
+		try {
+			int rated = (int) ratingObj;
+			rating = (double) rated;
+			return rating;
+		} catch (ClassCastException ex) {
+			rating = (double) ratingObj;
+			return rating;
+		}
 	}
 }
